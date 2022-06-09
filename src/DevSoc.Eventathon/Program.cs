d9916264d9c1eeeb2ef8f5d06e5aa13debdf23a0
LukeBillo <luke.o.billington@gmail.com>
2022-06-09 23:14:23 +0100

    Added React to back-end

--- src/DevSoc.Eventathon/Program.cs
+++ src/DevSoc.Eventathon/Program.cs
@@ -1,28 +1,36 @@
 namespace DevSoc.Eventathon;
 
-internal partial class Program
+internal class Program
 {
-    public static void Main(string[] args)
+    internal static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
         builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        if (!app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        
+        if (builder.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
 
         app.UseHttpsRedirection();
         app.UseAuthorization();
-        app.MapControllers();
+        
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+        app.MapControllerRoute("API controllers", "api/*");
+        app.MapFallbackToFile("index.html");
+        
         app.Run();
     }
 }
