c485f8bfbb0c8d23f36ac2f6e95aa1ed683856ac
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-06-12 00:57:50 +0100

    Added more DevSoc specific events for testing

--- src/DevSoc.Eventathon/ClientApp/src/resources/events.js
+++ src/DevSoc.Eventathon/ClientApp/src/resources/events.js
@@ -17,5 +17,29 @@
         title: 'Short event',
         start: new Date(2022, 5, 11, 11, 0, 0),
         end: new Date(2022, 5, 11, 14, 0, 0),
+    },
+    {
+        id: 3,
+        title: 'Groom Octogarf',
+        start: new Date(2022, 5, 15, 11, 0, 0),
+        end: new Date(2022, 5, 15, 14, 0, 0),
+    },
+    {
+        id: 4,
+        title: 'Fight God',
+        start: new Date(2022, 5, 15, 15, 0, 0),
+        end: new Date(2022, 5, 15, 17, 0, 0),
+    },
+    {
+        id: 5,
+        title: 'Open a cafe',
+        start: new Date(2022, 5, 13, 12, 0, 0),
+        end: new Date(2022, 5, 13, 18, 0, 0),
+    },
+    {
+        id: 6,
+        title: 'Find Tacky',
+        start: new Date(2022, 5, 14, 9, 0, 0),
+        end: new Date(2022, 5, 14, 13, 0, 0),
     }
 ];
