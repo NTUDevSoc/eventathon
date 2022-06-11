29908a8f9b2aec03bdfd4b25465c89d4675f00da
jordanofoster <JordanOFoster@outlook.com>
2022-06-11 16:57:16 +0100

    Scaffolding

---
+++ src/DevSoc.Eventathon/Controllers/CalController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevSoc.Eventathon.Controllers;
+
+[ApiController]
+public class CalController
+{
+    [HttpGet("api/calendar")]
+
+    public Task<OkResult> GetCal()
+    {
+        /* more JSON, probably! */
+        return Task.FromResult(new OkResult());
+    }
+}
