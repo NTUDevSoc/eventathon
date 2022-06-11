27bc561821069b26e8dedb55db2a3f5fc77e953f
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-06-11 20:39:26 +0100

    Makes a post request with an example JSON string

--- src/DevSoc.Eventathon/ClientApp/src/components/EventsCalendar.js
+++ src/DevSoc.Eventathon/ClientApp/src/components/EventsCalendar.js
@@ -3,6 +3,7 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import events from '../resources/events'
+import axios from "axios";
 
 const localizer = momentLocalizer(moment);
 
@@ -12,11 +13,15 @@ export const EventsCalendar = () => {
     
     const handleSelectSlot = useCallback(
         ({ start, end }) => {
-            const title = window.prompt('New Event name')
+            const title = window.prompt('Enter event name: ')
             console.log(start)
             if (title) {
                 setEvents((prev) => [...prev, { start, end, title }])
             }
+            
+            const element = document.querySelector('#post-request .article-id');
+            const article = { example: 'example data' };
+            axios.post('api/test', article).then(response => element.innerHTML = response.data.id);
         },
         [setEvents]
     )
@@ -33,6 +38,7 @@ export const EventsCalendar = () => {
                If you're a committee member you can also schedule new events from this page. 
                If you can schedule events and you're not a committtee member this is a bug.
                Be a good person and just don't use this immense power.</p>
+            <hr></hr>
             <Calendar
                 localizer={localizer} 
                 events={myEventsList}
