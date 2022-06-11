756dd40428851e42f4cb272f24cf92ddef9f4267
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-06-11 16:34:41 +0100

    CSS added (month view appears broken but day and week are fine)

--- src/DevSoc.Eventathon/ClientApp/src/components/MyCalendar.js
+++ src/DevSoc.Eventathon/ClientApp/src/components/MyCalendar.js
@@ -2,10 +2,11 @@
 
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 // Setup the localizer by providing the moment (or globalize, or Luxon) Object
 // to the correct localizer.
-const localizer = momentLocalizer(moment) // or globalizeLocalizer
+const localizer = momentLocalizer(moment) // or globalize Localizer
 
 export const MyCalendar = () => {
     return (
