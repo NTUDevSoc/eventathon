7be50fdacec84094eb36da2fbe1c3ac79b1938a9
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-06-11 18:11:57 +0100

    Can now create new events

--- src/DevSoc.Eventathon/ClientApp/src/components/EventsCalendar.js
+++ src/DevSoc.Eventathon/ClientApp/src/components/EventsCalendar.js
@@ -1,34 +1,33 @@
-﻿import React, { useState } from 'react';
+﻿import React, { Fragment, useState, useCallback, useMemo } from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import events from '../resources/events'
 
 const localizer = momentLocalizer(moment);
 
-const myEventsList = [
-    {
-        id: 0,
-        title: 'All Day Event',
-        allDay: true,
-        start: new Date(2022, 5, 11),
-        end: new Date(2022, 5, 12),
-      },
-      {
-        id: 1,
-        title: 'Long Event',
-        start: new Date(2022, 5, 10),
-        end: new Date(2015, 5, 13),
-      },
-      {
-        id: 2,
-        title: 'Short event',
-        start: new Date(2022, 5, 11, 11, 0, 0),
-        end: new Date(2022, 5, 11, 14, 0, 0),
-      }
-];
+
 
 
 export const EventsCalendar = () => {
+    const [myEventsList, setEvents] = useState(events)
+    
+    const handleSelectSlot = useCallback(
+        ({ start, end }) => {
+            const title = window.prompt('New Event name')
+            console.log(start)
+            if (title) {
+                setEvents((prev) => [...prev, { start, end, title }])
+            }
+        },
+        [setEvents]
+    )
+
+    const handleSelectEvent = useCallback(
+        (event) => window.alert(event.title),
+        []
+    )
+    
     return (
         <div>
             <h1>BASIC CALENDAR PAGE EXAMPLE!</h1>
@@ -37,6 +36,9 @@ export const EventsCalendar = () => {
                 events={myEventsList}
                 startAccessor="start"
                 endAccessor="end"
+                onSelectSlot={handleSelectSlot}
+                onSelectEvent={handleSelectEvent}
+                selectable
             />
         </div>
     );
---
+++ src/DevSoc.Eventathon/ClientApp/src/resources/events.js
@@ -0,0 +1,21 @@
+﻿export default [
+    {
+        id: 0,
+        title: 'All Day Event',
+        allDay: true,
+        start: new Date(2022, 5, 11),
+        end: new Date(2022, 5, 12),
+    },
+    {
+        id: 1,
+        title: 'Long Event',
+        start: new Date(2022, 5, 10),
+        end: new Date(2022, 5, 13),
+    },
+    {
+        id: 2,
+        title: 'Short event',
+        start: new Date(2022, 5, 11, 11, 0, 0),
+        end: new Date(2022, 5, 11, 14, 0, 0),
+    }
+];
