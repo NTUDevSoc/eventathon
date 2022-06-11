a045c1b5e7e0a2bbdfb8c869c864c1a22263b09f
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-06-11 19:17:59 +0100

    Added some extra text to the events calendar GUI

--- src/DevSoc.Eventathon/ClientApp/src/components/EventsCalendar.js
+++ src/DevSoc.Eventathon/ClientApp/src/components/EventsCalendar.js
@@ -1,12 +1,10 @@
-﻿import React, { Fragment, useState, useCallback, useMemo } from 'react'
+﻿import React, { useState, useCallback } from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import events from '../resources/events'
 
 const localizer = momentLocalizer(moment);
-
-
 
 
 export const EventsCalendar = () => {
@@ -30,7 +28,11 @@ export const EventsCalendar = () => {
     
     return (
         <div>
-            <h1>BASIC CALENDAR PAGE EXAMPLE!</h1>
+            <h1>Events Calendar!</h1>
+            <p>Welcome to the DevSoc events calendar. Here you can see all scheduled events. 
+               If you're a committee member you can also schedule new events from this page. 
+               If you can schedule events and you're not a committtee member this is a bug.
+               Be a good person and just don't use this immense power.</p>
             <Calendar
                 localizer={localizer} 
                 events={myEventsList}
