e6c19b03df830a18e9da8a0e297ad80638102e8d
Stephen Anderson <stephen.anderson0220@gmail.com>
2022-06-12 18:30:40 +0100

    Removes unnecessary wrapper from event data responses

--- src/DevSoc.Eventathon/Models/EventListRestResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using DevSoc.Eventathon.Models;
-namespace DevSoc.Eventathon.Calendars.Models;
-
-public class EventListRestResponse
-{
-    public EventResponse[] events { get; set; }
-    
-}
--- src/DevSoc.Eventathon/Models/EventRestResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using DevSoc.Eventathon.Models;
-namespace DevSoc.Eventathon.Calendars.Models;
-
-public class EventRestResponse
-{
-    public EventResponse @event { get; set; }
-    
-}
