58848e88a731a9ee071904bbcaee33fba46be260
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-06-12 15:37:00 +0100

    My Events is now accessed through the nav bar

--- src/DevSoc.Eventathon/ClientApp/src/components/UsersEvents.js
+++ src/DevSoc.Eventathon/ClientApp/src/components/UsersEvents.js
@@ -2,20 +2,9 @@
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import events from '../resources/events'
-import React, { useState, useCallback } from 'react'
-import axios from "axios";
+import React, { useState } from 'react'
 
 const localizer = momentLocalizer(moment);
-
-const ConfirmAttendance = () => {
-    const element = document.querySelector('#post-request .article-id');
-    const article = {
-        userID: 107,
-        eventID: 162,
-        name: "Arbitrarily Chosen Name Here",
-    };
-    axios.post('api/attendance', article).then(response => element.innerHTML = response.data.id);
-}
 
 export const UsersEvents = () => {
     const [myEventsList] = useState(events)
@@ -23,16 +12,12 @@ export const UsersEvents = () => {
     return (
         <div>
             <h1><u>My Events!</u></h1>
-
-            <button className="btn btn-primary" onClick={ConfirmAttendance}>Register Attendance</button>
-            
             <p>Here you will see all scheduled events you are attending:</p>
             <Calendar
                 localizer={localizer}
                 events={myEventsList}
                 startAccessor="start"
                 endAccessor="end"
-                selectable
             />
          </div>
     );
