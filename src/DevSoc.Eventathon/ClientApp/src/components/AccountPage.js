3105caf9c1943ad2a031184390ca3b615dfaefa7
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-06-14 16:27:49 +0100

    background colour now extends past the boundaries of the form

--- src/DevSoc.Eventathon/ClientApp/src/components/AccountPage.js
+++ src/DevSoc.Eventathon/ClientApp/src/components/AccountPage.js
@@ -4,15 +4,18 @@ import SignupForm from "../forms/SignupForm";
 
 export const AccountPage = () => {
     return (
-        <div className="container-fluid">
-            <div className="row justify-content-center">
+        <div className="row justify-content-center">
 
-                <div className="col-md-6">
+            <div className="col-md-6">
+                <div>
                     <h1 className="mt-5 d-flex justify-content-center"><u>Login!</u></h1>
                     <LoginForm />
                 </div>
+                
+            </div>
 
-                <div className="col-md-6 red-form">
+            <div className="col-md-6 red-form">
+                <div className="right-side">
                     <h1 className="mt-5 d-flex justify-content-center"><u>Signup!</u></h1>
                     <SignupForm />
                 </div>
