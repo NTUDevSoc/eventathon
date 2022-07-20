4e1b43ae729b3076623a857884b451bd602ed618
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-07-20 22:01:47 +0100

    Bug Fixed - invalid signup password error now displayed + creation successful message now displayed

--- src/DevSoc.Eventathon/ClientApp/src/forms/SignupForm.js
+++ src/DevSoc.Eventathon/ClientApp/src/forms/SignupForm.js
@@ -4,13 +4,16 @@ import {createUser} from "../api/events-endpoint";
 
 export default function SignupForm() {
 
-    const { register, handleSubmit, errors, setError } = useForm();
+    const { register, handleSubmit, setError, formState: { errors } } = useForm();
 
     const onSubmit = useCallback(
         (formValues) => {
             createUser(formValues.username, formValues.password, formValues.displayName).then(isSignedUp => {
                 if (!isSignedUp) {
                     setError('auth', { type: 'custom', message: 'Your password was incorrect' })
+                }
+                else{
+                    alert("Account Creation Successful");
                 }
             })
         },
@@ -26,7 +29,7 @@ export default function SignupForm() {
                    type="password"
                    {...register("password", { required: true, minLength: 8 })}
             />
-{/*            {errors.password && <p> Password must contain more than seven characters</p>}*/}
+            {errors.password && <p> Password must contain more than seven characters</p>}
     
             <input className="form-field" placeholder="Display name"
                    {...register("displayName", { required: true})}
